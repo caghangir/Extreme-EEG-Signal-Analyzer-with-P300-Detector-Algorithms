078ffb8bbe1b4d477664b23870986623d751f7ee
Çağatay Demirel <caghangir@gmail.com>
2019-02-04 20:30:41 +0300

    Update extremeEEGSignalAnalyzer.py

--- extremeEEGSignalAnalyzer.py
+++ extremeEEGSignalAnalyzer.py
@@ -98,16 +98,7 @@ def binPower(signal, Band, Fs):
     return power, power_ratio
 
 def pfd(X, D=None):
-    """Compute Petrosian Fractal Dimension of a time series from either two
-    cases below:
-        1. X, the time series of type list (default)
-        2. D, the first order differential sequence of X (if D is provided,
-           recommended to speed up)
-    In case 1, D is computed using Numpy's difference function.
-    To speed up, it is recommended to compute D before calling this function
-    because D may also be used by other functions whereas computing it here
-    again will slow down.
-    """
+    """Compute Petrosian Fractal Dimension """"
     if D is None:
         D = np.diff(X)
         D = D.tolist()
@@ -140,30 +131,7 @@ def hfd(X, Kmax):
     return p[0]
 
 def hjorth(X, D=None):
-    """ Compute Hjorth mobility and complexity of a time series from either two
-    cases below:
-        1. X, the time series of type list (default)
-        2. D, a first order differential sequence of X (if D is provided,
-           recommended to speed up)
-    In case 1, D is computed using Numpy's Difference function.
-    Notes
-    -----
-    To speed up, it is recommended to compute D before calling this function
-    because D may also be used by other functions whereas computing it here
-    again will slow down.
-    Parameters
-    ----------
-    X
-        list
-        a time series
-    D
-        list
-        first order differential sequence of a time series
-    Returns
-    -------
-    As indicated in return line
-    Hjorth mobility and complexity
-    """
+    """ Compute Hjorth mobility and complexity of a time series """
 
     if D is None:
         D = np.diff(X)
@@ -184,29 +152,7 @@ def hjorth(X, D=None):
     return np.sqrt(M2 / TP), np.sqrt(float(M4) * TP / M2 / M2)  # Hjorth Mobility and Complexity
 
 def hurst(X):
-    """ Compute the Hurst exponent of X. If the output H=0.5,the behavior
-    of the time-series is similar to random walk. If H<0.5, the time-series
-    cover less "distance" than a random walk, vice verse.
-    Parameters
-    ----------
-    X
-        list
-        a time series
-    Returns
-    -------
-    H
-        float
-        Hurst exponent
-    Notes
-    --------
-    Author of this function is Xin Liu
-    Examples
-    --------
-    >>> import pyeeg
-    >>> from numpy.random import randn
-    >>> a = randn(4096)
-    >>> pyeeg.hurst(a)
-    0.5057444
+    """ Compute the Hurst exponent of X. 
     """
     X = np.array(X)
     N = X.size
@@ -231,72 +177,7 @@ def hurst(X):
     return H
     
 def dfa(X, Ave=None, L=None):
-    """Compute Detrended Fluctuation Analysis from a time series X and length of
-    boxes L.
-    The first step to compute DFA is to integrate the signal. Let original
-    series be X= [x(1), x(2), ..., x(N)].
-    The integrated signal Y = [y(1), y(2), ..., y(N)] is obtained as follows
-    y(k) = \sum_{i=1}^{k}{x(i)-Ave} where Ave is the mean of X.
-    The second step is to partition/slice/segment the integrated sequence Y
-    into boxes. At least two boxes are needed for computing DFA. Box sizes are
-    specified by the L argument of this function. By default, it is from 1/5 of
-    signal length to one (x-5)-th of the signal length, where x is the nearest
-    power of 2 from the length of the signal, i.e., 1/16, 1/32, 1/64, 1/128,
-    ...
-    In each box, a linear least square fitting is employed on data in the box.
-    Denote the series on fitted line as Yn. Its k-th elements, yn(k),
-    corresponds to y(k).
-    For fitting in each box, there is a residue, the sum of squares of all
-    offsets, difference between actual points and points on fitted line.
-    F(n) denotes the square root of average total residue in all boxes when box
-    length is n, thus
-    Total_Residue = \sum_{k=1}^{N}{(y(k)-yn(k))}
-    F(n) = \sqrt(Total_Residue/N)
-    The computing to F(n) is carried out for every box length n. Therefore, a
-    relationship between n and F(n) can be obtained. In general, F(n) increases
-    when n increases.
-    Finally, the relationship between F(n) and n is analyzed. A least square
-    fitting is performed between log(F(n)) and log(n). The slope of the fitting
-    line is the DFA value, denoted as Alpha. To white noise, Alpha should be
-    0.5. Higher level of signal complexity is related to higher Alpha.
-    Parameters
-    ----------
-    X:
-        1-D Python list or numpy array
-        a time series
-    Ave:
-        integer, optional
-        The average value of the time series
-    L:
-        1-D Python list of integers
-        A list of box size, integers in ascending order
-    Returns
-    -------
-    Alpha:
-        integer
-        the result of DFA analysis, thus the slope of fitting line of log(F(n))
-        vs. log(n). where n is the
-    Examples
-    --------
-    >>> import pyeeg
-    >>> from numpy.random import randn
-    >>> print(pyeeg.dfa(randn(4096)))
-    0.490035110345
-    Reference
-    ---------
-    Peng C-K, Havlin S, Stanley HE, Goldberger AL. Quantification of scaling
-    exponents and crossover phenomena in nonstationary heartbeat time series.
-    _Chaos_ 1995;5:82-87
-    Notes
-    -----
-    This value depends on the box sizes very much. When the input is a white
-    noise, this value should be 0.5. But, some choices on box sizes can lead to
-    the value lower or higher than 0.5, e.g. 0.38 or 0.58.
-    Based on many test, I set the box sizes from 1/5 of    signal length to one
-    (x-5)-th of the signal length, where x is the nearest power of 2 from the
-    length of the signal, i.e., 1/16, 1/32, 1/64, 1/128, ...
-    You may generate a list of box sizes and pass in such a list as a
-    parameter.
+    """Compute Detrended Fluctuation Analysis 
     """
 
     X = np.array(X)
